59da2418330280d934b342477a686b3077b83f27
UNDEFIN3D7910 <diogo.souza.marques@escola.pr.gov.br>
2025-08-28 09:04:45 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,18 @@
+//muda o nome a partir do Seletor de classe
+
+var pacientes = document.querySelector(".subtitulo")
+var Alanzoka = document.querySelector(".titulo");
+Alanzoka.textContent = "Alanzoka Nutrição";
+pacientes.textContent = "Meus Pacientes";
+
+//acessar a tag tr - paciente paulo
+
+var paciente = document.querySelector("primeiro-paciente");
+var TdPeso = paciente.querySelector(".info-peso");
+var peso = textContent;
+console.log(peso);
+
+
+var TdAltura = paciente.querySelector(".info-altura");
+var altura = textContent;
+console.log(altura);
