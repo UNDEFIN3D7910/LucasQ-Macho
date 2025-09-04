c3aeaf19ea4871ecb6c7aa4aeaa1cdf164dbade7
UNDEFIN3D7910 <diogo.souza.marques@escola.pr.gov.br>
2025-09-04 08:59:01 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,18 +1,99 @@
 //muda o nome a partir do Seletor de classe
 
-var pacientes = document.querySelector(".subtitulo")
+var subtitulo = document.querySelector(".subtitulo")
 var Alanzoka = document.querySelector(".titulo");
 Alanzoka.textContent = "Alanzoka Nutrição";
-pacientes.textContent = "Meus Pacientes";
+subtitulo.textContent = "Meus Pacientes";
 
-//acessar a tag tr - paciente paulo
+//acessar a tag tr - paciente Paulo
 
-var paciente = document.querySelector("primeiro-paciente");
+var paciente = document.querySelector("#primeiro-paciente");
+var TdPeso = paciente.querySelector(".info-peso");
+var peso = TdPeso.textContent;
+
+
+var TdAltura = paciente.querySelector(".info-altura");
+var altura = TdAltura.textContent;
+
+//acessar a tag tr - paciente João
+
+var paciente = document.querySelector("#segundo-paciente");
 var TdPeso = paciente.querySelector(".info-peso");
 var peso = textContent;
-console.log(peso);
 
 
 var TdAltura = paciente.querySelector(".info-altura");
 var altura = textContent;
-console.log(altura);
+
+
+//acessar a tag tr - paciente Erica
+
+var paciente = document.querySelector("#terceiro-paciente");
+var TdPeso = paciente.querySelector(".info-peso");
+var peso = textContent;
+
+
+var TdAltura = paciente.querySelector(".info-altura");
+var altura = textContent;
+
+
+//acessar a tag tr - paciente Douglas
+
+var paciente = document.querySelector("#quarto-paciente");
+var TdPeso = paciente.querySelector(".info-peso");
+var peso = textContent;
+
+
+var TdAltura = paciente.querySelector(".info-altura");
+var altura = textContent;
+
+
+//acessar a tag tr - paciente Tatiana
+
+var paciente = document.querySelector("#quinto-paciente");
+var TdPeso = paciente.querySelector(".info-peso");
+var peso = textContent;
+
+
+var TdAltura = paciente.querySelector(".info-altura");
+var altura = textContent;
+
+var PesoValido = true;
+var AlturaValida = true;
+
+if (PesoValido && AlturaValida){
+    //acessa e altera o imc
+var imc = peso / (altura * altura);
+
+var tdImc = paciente.querySelector(".info-imc");
+TdImc.textContent = imc;
+}
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso Inválido");
+    PesoValido = false;
+    tdImc.textContent = "Peso Inválido";
+}
+
+if(altura <= 0 || altura >= 4.00){
+    console.log("Altura Inválida");
+    AlturaValida = false;
+    tdImc.textContent = "Altura Inválida";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
